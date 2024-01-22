ac6ca436f326298cf85ddf83fff7c5c389ee5188
pranshu6sept <128319537+pranshu6sept@users.noreply.github.com>
2024-01-22 18:32:42 +0000

    Date and Time Complted

---
+++ 01_Basics/06_nums_and_maths.js
@@ -0,0 +1,30 @@
+const score = 400
+
+const balance = new Number(100)
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+const otherNumber =123.8966
+// console.log(otherNumber.toPrecision(4));
+
+const hunderds = 100000
+// console.log(hunderds.toLocaleString('en-IN'));
+
+console.log(Math);
+console.log(Math.abs(-4));
+console.log(Math.round(4.6));
+console.log(Math.ceil(4.3));
+console.log(Math.floor(4.6));
+console.log(Math.max(4,5,6,7));
+console.log(Math.min(4,5,6,7));
+
+console.log(Math.random());
+console.log(Math.floor(Math.random()*10) +1);
+
+const min = 10
+const max = 20
+
+console.log(Math.random()*(max-min + 1) + min)
+
