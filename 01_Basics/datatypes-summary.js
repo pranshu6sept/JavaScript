4473c8a4c91af90012ceb8921fb72bdc131f6d38
pranshu6sept <128319537+pranshu6sept@users.noreply.github.com>
2024-01-22 17:22:44 +0000

    string completed

--- 01_Basics/datatypes-summary.js
+++ 01_Basics/datatypes-summary.js
@@ -21,7 +21,7 @@ const BigNumber = 33700999888888888888889n
 const heros = ["Shaktiman","Nagraj","Doga"]
 
 let myobj = {
-    name :"Pranshu"
+    name :"Pranshu",
     age : 29
 }
 
@@ -32,3 +32,25 @@ const myfunction= function(){
 console.log(typeof bigNumber)
 
 // https://262.ecma-international.org/5.1/#sec-11.4.3
+
+// ++++++++++++++++++++++++++++++++++++++++++++++++
+
+// Stack(Primitive),Heap(Non-Primitive)
+
+let myYoutubeName = "TechGeeks"
+let anothername = myYoutubeName
+anothername = "chaiaurcode"
+
+let user1 ={
+    email : "user@google.com",
+    upi : "xyz@upi"
+}
+
+let user2 = user1
+
+user2.email ="luffy@strawhats.com"
+
+console.log(user1.email);
+console.log(user2.email);
+
+
