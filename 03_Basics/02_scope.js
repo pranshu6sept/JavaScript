e30b3cfe79c8f36014dd498a10b96c629a301a27
pranshu6sept <128319537+pranshu6sept@users.noreply.github.com>
2024-01-23 20:00:33 +0000

    scope

--- 03_Basics/02_scope.js
+++ 03_Basics/02_scope.js
@@ -7,9 +7,52 @@ if (true){
     console.log("Inner:", a);
 }
 
-for (let i = 0; i< Array.length; i++){
-    const element = array[i];
-}
-console.log(a);
+// for (let i = 0; i< Array.length; i++){
+//     const element = array[i];
+// }
+// console.log(a);
 // console.log(b);
 // console.log(c);
+
+function one(){
+    const username = "hitesh"
+
+    function two(){
+        const website = "youtube"
+        console.log(username);
+    }
+    // console.log(website);
+
+    two()
+}
+
+// one()
+
+if (true){
+    const username = "hitesh"
+    if (username === "hitesh"){
+        const website = "Youtube"
+        console.log(username + website);
+    }
+    // console.log(website);
+}
+
+// console.log(username)
+
+// ++++++++++ intersting ++++++++++++
+
+console.log(addone(5));
+
+function addone(num){
+    return num + 1
+}
+
+addone(5)
+
+console.log(addTwo(2))
+
+const addTwo = function(num){
+    return num + 2
+}
+
+addTwo(5)
